c47a0230400eeb6a3bbb8d40ece566c20f48686d
Haley Bryant <Haleye.bryant@gmail.com>
2019-06-04 14:29:15 -0400

    created my address file

---
+++ python/my_address.py
@@ -0,0 +1,4 @@
+my_address = {"name": "Haley Bryant", "street": "400 Galloway St. NE", "city": "Washington", "state": " DC", "zip": " 20011"}
+print(my_address["name"])
+print(my_address["street"])
+print(my_address["city"] + my_address["state"] + my_address ["zip"])
